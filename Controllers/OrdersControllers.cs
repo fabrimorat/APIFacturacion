49c8534a1d4bc0401bbb5c82643a7b768e01c416
fabri <fabri@DESKTOP-8L5TLOD>
2024-06-21 10:02:23 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/OrdersControllers.cs
@@ -0,0 +1,36 @@
+﻿using APIFacturacion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace APIFacturacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private static List<Order> _orders = new List<Order>();
+
+        [HttpPost]
+        public ActionResult<Order> CreateOrder([FromBody] Order order)
+        {
+            _orders.Add(order);
+            return CreatedAtAction(nameof(GetOrder), new { id = _orders.Count - 1 }, order);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> GetAllOrders()
+        {
+            return _orders;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetOrder(int id)
+        {
+            if (id < 0 || id >= _orders.Count)
+            {
+                return NotFound();
+            }
+            return _orders[id];
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace APIFacturacion.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string PokemonName { get; set; }
+        public int Quantity { get; set; }
+        public string CustomerInfo { get; set; }
+    }
+}
